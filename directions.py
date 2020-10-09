9f6d3af0acc9ecd7f71a9b72b29c1a1c24fd3a92
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-10-09 08:30:02 -0700

    secret word

--- directions.py
+++ directions.py
@@ -1,12 +1,17 @@
 import requests
+import configparser
+config = configparser.ConfigParser()
+config.read("credentials.ini")
 
-def get_name(lat, long, state, key):
-    origen = "http://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest/?lat=" + str(lat) + "&lon=" + str(long) + "&state=" + str(state) + "&apikey=" + key + "&format=json&notStore=false&version=4.10"#service provided by tamu.
+location = [44.587662,-123.256691]
+
+def get_name(lat, long, state):
+    origen = "http://geoservices.tamu.edu/Services/ReverseGeocoding/WebService/v04_01/Rest/?lat=" + str(lat) + "&lon=" + str(long) + "&state=" + str(state) + "&apikey=" + config["DEFAULT"]["key_tamu"] + "&format=json&notStore=false&version=4.10"#service provided by tamu.
 
     answer = ""#storage only the name of the street.
     response = requests.get(origen).json()
     result = response['StreetAddresses'][0]['StreetAddress']#only gets the address of the avenue or street
-
+    # print(response)
     # remove the addres number, leaving only the street or avenue name.
     signal = 0
     for i in result:
@@ -16,3 +21,5 @@ def get_name(lat, long, state, key):
             answer += i
 
     return answer[1:]
+
+print(get_name(location[0],location[1],"or"))
--- google.py
+++ google.py
@@ -1,44 +1,14 @@
 import googlemaps
 import json
 
+import configparser#import the key from secret file
+config = configparser.ConfigParser()
+config.read("credentials.ini")
 #google version getting name
-
-location = [44.660458,-123.280029]
-
+# location = [44.587662,-123.256691]#for testing
 def get_name(lat, long):
-    gmaps = googlemaps.Client(key='AIzaSyB1ym7awZxyyWrmI5Y7OTAQDEyasZZMG0o')
+    gmaps = googlemaps.Client(key=config["DEFAULT"]["key_google"])
     result = gmaps.reverse_geocode((lat, long))
     full_address = result[3]["address_components"][1]["short_name"]
-    # signal = 0
-    # text =  full_address.split(",")
-    # resultado = text[0].split(" ")
     return full_address
-    # return ' '.join(resultado[1:])
-
-print(get_name(location[0],location[1]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import requests
-#
-# def get_name(lat, long):
-#     origen = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(long) + "&key=AIzaSyB1ym7awZxyyWrmI5Y7OTAQDEyasZZMG0o"
-#
-#     response = requests.get(origen).json()
-#     print(response["results"][0]["address_components"][1]['short_name'])
-#
-# get_name(44.587662,-123.256691)
+# print(get_name(location[0],location[1]))
