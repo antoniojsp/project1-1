3fbd2333ce1eece296e86ce58e72252d084f53d6
Wil Sprouse <wils@testium.cs.uoregon.edu>
2020-10-13 17:15:49 -0700

    Update

--- flask_src/src/app.py
+++ flask_src/src/app.py
@@ -11,7 +11,7 @@ config = configparser.ConfigParser()
 config.read("credentials.ini")
 ALLOWED_EXTENSIONS = set(['txt'])
 
-#uploads_dir = os.path.join(app.instance_path, '/upload_folder')
+#uploads_dir = os.path.join(config['DEFAULT']['UPLOAD_FOLDER'],  'upload_folder')
 #os.makedirs(uploads_dir, exist_ok=True)
 
 
@@ -21,30 +21,25 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+@app.route('/viewfile/<filename>')
+def viewfile(filename):
+    with open('./'+filename) as f:
+        return f.read().strip()
+
+
 """Login Page Logic"""
 @app.route("/", methods=["GET", "POST"])
 def get():
-
     if request.method == 'POST':
-        #if request.form['submitted_data']: # or request.form['password'] != 'admin':
-        #    return request.form['submitted_data']
-        app.logger.debug('Juicy')
         file_ = request.files['submitted_data']
-        app.logger.debug('Juiced')
         if file_.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file_ and allowed_file(file_.filename):
             filename = secure_filename(file_.filename)
-            with open(filename) as f:
-                app.logger.debug(f)
-            #file_.save(os.path.join(uploads_dir, filename))
-            #file_.save(os.path.join(config['DEFAULT']['UPLOAD_FOLDER'], str(filename)))
-            return redirect(url_for('/',
-                                    filename=filename))
-
-
-
+            file_.save(filename)
+            return redirect(url_for('viewfile', filename=filename))
     return render_template('submit_data.html')
 
 
