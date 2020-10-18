bbd2c52f97f6c1e11461b6107534df46ca6a0464
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-10-18 15:32:28 -0700

    last

--- google.py
+++ google.py
@@ -1,14 +1,18 @@
 import googlemaps
 import json
+import time
+start_time = time.time()
 
 import configparser#import the key from secret file
 config = configparser.ConfigParser()
 config.read("credentials.ini")#stored keys outside respository.
 #google version getting name. Uses googlemaps module. and the key is kept secret
-# location = [ 44.628635,-123.240303]#for testing
+location = [44.58836,-123.262474]#for testing
 def get_name_google(lat, long):
     gmaps = googlemaps.Client(key=config["DEFAULT"]["key_google"])
+
     result = gmaps.reverse_geocode((lat, long))
+    
     #check for the right name in the json file
     abrev = ["Rd", "Ln", "St", "Ave", "Dr", "Way", "Pl", "Blvd", "Ct", "Terrace", "I-", "Hwy", "Cir", "Aly", "Bldg","Expy", "Fwy", "Gtwy", "Pl", "Ste", "Vw", "Sta"]
 
@@ -19,8 +23,9 @@ def get_name_google(lat, long):
             if any(x in result[i]["address_components"][1]["short_name"] for x in abrev):
                 full_address = result[i]["address_components"][1]["short_name"]
         except:
-            full_address = result[0]["address_components"][1]["short_name"]
+            full_address = result[1]["address_components"][1]["short_name"]
 
     return full_address
 
 # print(get_name_google(location[0],location[1]))
+# print("--- %s seconds ---" % (time.time() - start_time))
--- pool_street.py
+++ pool_street.py
@@ -13,8 +13,8 @@ parseado = gpxpy.parse(file)
 position = get_points(parseado)#array of objects that hold information like lat, long, elev, time.
 
 
-start = 0#first point
-end = len(position)-1#last point
+start = 70#first point
+end = 200#last point
 print("Start: {} End:{}".format(start,end))#debug
 
 pool = [""]*len(position)#cache to reduce the number of requests
@@ -26,17 +26,14 @@ def change(list, start, end, storage):#list with all
     pool1 = ThreadPool(processes=1)
 
     if pool[start] == "":
-        async_result = pool1.apply_async(get_name_google, (list[start].get_lat(), list[start].get_long()))
-        # pool[start] = get_name_google(list[start].get_lat(), list[start].get_long())
+        async_result = pool1.apply_async(get_name_google, (list[start].get_lat(), list[start].get_long()),)
         pool[start] = async_result.get()
     if pool[middle] == "":
         async_result1 = pool1.apply_async(get_name_google, (list[middle].get_lat(), list[middle].get_long()))
         pool[middle] = async_result1.get()
-        # pool[middle] = get_name_google(list[middle].get_lat(), list[middle].get_long())
     if pool[end] == "":
         async_result2 = pool1.apply_async(get_name_google, (list[end].get_lat(), list[end].get_long()))
         pool[end] = async_result2.get()
-        # pool[end] = get_name_google(list[end].get_lat(), list[end].get_long())
 
     if pool[start] == pool[middle] and pool[middle] == pool[end]:
         return
@@ -44,18 +41,14 @@ def change(list, start, end, storage):#list with all
     elif (pool[start] == pool[middle] and  pool[middle] != pool[end]) or (pool[start] != pool[middle] and  pool[middle] == pool[end]):
 
         if pool[middle+1] == "":
-            # pool[middle+1] = get_name_google(list[middle+1].get_lat(), list[middle+1].get_long())
             async_result3 = pool1.apply_async(get_name_google, (list[middle+1].get_lat(), list[middle+1].get_long()))
             pool[middle+1] = async_result3.get()
         if pool[middle-1] == "":
-            # pool[middle-1] = get_name_google(list[middle-1].get_lat(), list[middle-1].get_long())
             async_result4 = pool1.apply_async(get_name_google, (list[middle-1].get_lat(), list[middle-1].get_long()))
             pool[middle-1] = async_result4.get()
 
         if pool[middle] != pool[middle-1] or pool[middle] != pool[middle+1]:
-            # print("Street now{}".format(pool[middle]))
             if len(storage) == 0:
-                # saving.append(Holder(middle, pool[middle-1]))
                 if pool[middle-1] not in storage:
                     storage.append((middle,pool[middle-1]))
             else:
@@ -73,17 +66,15 @@ def change(list, start, end, storage):#list with all
 
 intersection = []
 change(position, start, end, intersection)
-#
-# print([str(calles[i.get_index()-1]) +" "+ str(calles[i.get_index()])  +" "+ str(calles[i.get_index()+1]) for i in intersection])
-#
+
+
 j = 0
 for i in pool:
     if i != "":
         j+=1
 print(j)
-# print(pool)
-
+print(len(intersection))
 print([i[1] for i in intersection] )
+print([i[0] for i in intersection] )
 
-# print([i.get_index() for i in intersection] )
 print("--- %s seconds ---" % (time.time() - start_time))
