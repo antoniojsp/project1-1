3a7d89607b83e90254988ef8b87bb836a9c57af0
Antonio Silva Paucar <antonios@uoreogon.eedu>
2020-10-18 09:23:46 -0700

    pool_street.py hyperthreat

--- pool_street.py
+++ pool_street.py
@@ -2,35 +2,41 @@ from google import get_name_google
 from getgpx import get_points
 from distance import distance
 import gpxpy
+import time
+
+from multiprocessing.pool import ThreadPool#multi process
+
+start_time = time.time()#record time
 
 file = open('09_27_20.gpx', 'r')
 parseado = gpxpy.parse(file)
 position = get_points(parseado)#array of objects that hold information like lat, long, elev, time.
 
-class Holder:
-    def __init__(self, index, name):
-        self.__index = index
-        self.__name = name
-    def get_index(self):
-        return self.__index
-    def get_name(self):
-        return self.__name
 
-start = 0
-end = len(position)-1
-print("Start: {} End:{}".format(start,end))
+start = 0#first point
+end = len(position)-1#last point
+print("Start: {} End:{}".format(start,end))#debug
 
-pool = [""]*len(position)
+pool = [""]*len(position)#cache to reduce the number of requests
+print(len(pool))
 
-def change(list, start, end, saving):
+def change(list, start, end, storage):#list with all
     middle = int((start+end)/2)
 
+    pool1 = ThreadPool(processes=1)
+
     if pool[start] == "":
-        pool[start] = get_name_google(list[start].get_lat(), list[start].get_long())
+        async_result = pool1.apply_async(get_name_google, (list[start].get_lat(), list[start].get_long()))
+        # pool[start] = get_name_google(list[start].get_lat(), list[start].get_long())
+        pool[start] = async_result.get()
     if pool[middle] == "":
-        pool[middle] = get_name_google(list[middle].get_lat(), list[middle].get_long())
+        async_result1 = pool1.apply_async(get_name_google, (list[middle].get_lat(), list[middle].get_long()))
+        pool[middle] = async_result1.get()
+        # pool[middle] = get_name_google(list[middle].get_lat(), list[middle].get_long())
     if pool[end] == "":
-        pool[end] = get_name_google(list[end].get_lat(), list[end].get_long())
+        async_result2 = pool1.apply_async(get_name_google, (list[end].get_lat(), list[end].get_long()))
+        pool[end] = async_result2.get()
+        # pool[end] = get_name_google(list[end].get_lat(), list[end].get_long())
 
     if pool[start] == pool[middle] and pool[middle] == pool[end]:
         return
@@ -38,32 +44,46 @@ def change(list, start, end, saving):
     elif (pool[start] == pool[middle] and  pool[middle] != pool[end]) or (pool[start] != pool[middle] and  pool[middle] == pool[end]):
 
         if pool[middle+1] == "":
-            print(middle+1)
-            pool[middle+1] = get_name_google(list[middle+1].get_lat(), list[middle+1].get_long())
+            # pool[middle+1] = get_name_google(list[middle+1].get_lat(), list[middle+1].get_long())
+            async_result3 = pool1.apply_async(get_name_google, (list[middle+1].get_lat(), list[middle+1].get_long()))
+            pool[middle+1] = async_result3.get()
         if pool[middle-1] == "":
-            pool[middle-1] = get_name_google(list[middle-1].get_lat(), list[middle-1].get_long())
+            # pool[middle-1] = get_name_google(list[middle-1].get_lat(), list[middle-1].get_long())
+            async_result4 = pool1.apply_async(get_name_google, (list[middle-1].get_lat(), list[middle-1].get_long()))
+            pool[middle-1] = async_result4.get()
 
         if pool[middle] != pool[middle-1] or pool[middle] != pool[middle+1]:
             # print("Street now{}".format(pool[middle]))
-            if len(saving) == 0:
-                saving.append(Holder(middle, pool[middle-1]))
+            if len(storage) == 0:
+                # saving.append(Holder(middle, pool[middle-1]))
+                if pool[middle-1] not in storage:
+                    storage.append((middle,pool[middle-1]))
             else:
-                if saving[len(saving)-1].get_name() == pool[middle-1]:
-                    saving.append(Holder(middle, pool[middle]))
+                if storage[len(storage)-1][1] == pool[middle-1]:
+                    storage.append((middle,pool[middle]))
                 else:
-                    saving.append(Holder(middle, pool[middle-1]))
+                    storage.append((middle,pool[middle-1]))
 
         else:
-            change(list,start, middle, saving)
-            change(list, middle, end, saving)
+            change(list,start, middle, storage)
+            change(list, middle, end, storage)
     else:
-        change(list,start, middle, saving)
-        change(list, middle, end, saving)
+        change(list,start, middle, storage)
+        change(list, middle, end, storage)
 
 intersection = []
 change(position, start, end, intersection)
 #
 # print([str(calles[i.get_index()-1]) +" "+ str(calles[i.get_index()])  +" "+ str(calles[i.get_index()+1]) for i in intersection])
 #
-print([i.get_name() for i in intersection] )
+j = 0
+for i in pool:
+    if i != "":
+        j+=1
+print(j)
+# print(pool)
+
+print([i[1] for i in intersection] )
+
 # print([i.get_index() for i in intersection] )
+print("--- %s seconds ---" % (time.time() - start_time))
